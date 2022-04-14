492e84791709b50979997fc2c682eb30d7fb2a9f
Joseph G Gonzalez Diaz <joseph.gonzalez_diaz@us.af.mil>
2022-04-14 13:27:33 -0500

    14APR22

--- src/solution/HelperSolution.ts
+++ src/solution/HelperSolution.ts
@@ -1,8 +1,35 @@
 export function encypher(codePhrase: string) {
-    // changes every r to n
-    // changes every s to a
-    // deletes second+ e
-    // changes second+ t to m
-    // changes every ph to lo
-    return codePhrase;
+    let newString = "";
+    let eCounter = 0;
+    let tCounter = 0;
+    for (let i = 0; i < codePhrase.length; i++){
+        const letter = codePhrase[i];
+        if (letter=="r"){
+            newString+="n";
+        } else if (letter=="s"){
+            newString+="a";
+        } else if (letter=="e"){
+            if (eCounter==0){
+                newString+="e";
+                eCounter+=1;
+            }
+        } else if (letter=="t"){
+            if (tCounter==0){
+                newString+="t";
+                tCounter+=1;
+            } else {
+                newString+="m";
+            }
+        } else if (letter=="p"){
+            if (i+1  < codePhrase.length && codePhrase[i+1]=="h"){
+                newString+="lo";
+                i++;
+            } else {
+                newString+="p";
+            }
+        } else {
+            newString+=letter;
+        }
+    }
+    return newString;
 }
